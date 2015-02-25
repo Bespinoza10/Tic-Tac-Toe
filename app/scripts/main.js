c74c1f3abc2b69f41763cc466d22307a77787003
Luke Lancaster <luketlancaster@gmail.com>
2015-02-25 13:09:03 -0600

    alternates piece value on clicks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,5 +7,9 @@ var board = ['','','','','','','','',''];
 $('tbody').on('click','td', function(event){
 	var $td = $(this).closest("td");
 	$td.text(piece);
-	console.log('works!');
+	if(piece === 'x') {
+		piece = 'o';
+	} else {
+		piece = 'x';
+	};
 });
