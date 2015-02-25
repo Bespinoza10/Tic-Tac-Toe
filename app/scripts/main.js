7132c3a4c9522e480282b0ce312368e7c8b02172
Luke Lancaster <luketlancaster@gmail.com>
2015-02-25 13:39:20 -0600

    store moves in array

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,4 +13,18 @@ $('tbody').on('click','td', function(event){
 	} else {
 		piece = 'x';
 	}
+board[0] = $('#one').text();
+board[1] = $('#two').text();
+board[2] = $('#three').text();
+board[3] = $('#four').text();
+board[4] = $('#five').text();
+board[5] = $('#six').text();
+board[6] = $('#seven').text();
+board[7] = $('#eight').text();
+board[8] = $('#nine').text();
 });
+
+
+//check td #id for value
+//store value in board array
+
