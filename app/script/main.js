1a62944cd0c3e46861a8d78866665b6d08ea8b7e
Brandon Espinoza <espinozab100@gmail.com>
2015-02-25 12:42:36 -0600

    script directory added

---
+++ app/script/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var fb = new Firebase ('https://ticytacytoey.firebaseio.com/');
+var piece = 'x';
+
+var board = ['','','','','','','','',''];
+$('tbody').on('click','td', function(event){
+	var $td = $(this).closest("td");
+	$td.text(piece);
+});
