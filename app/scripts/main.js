d99a29dafff21de081c274962c7213c9e5b92edb
Luke Lancaster <luketlancaster@gmail.com>
2015-02-25 21:49:10 -0600

    check for win in place - only works the move after, need to fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,41 +1,73 @@
 'use strict'
 
 var fb    = new Firebase ('https://ticytacytoey.firebaseio.com/'),
-    piece = 'x',
+    piece = 'X',
     board = {a1:'', a2:'', a3:'', b1:'', b2:'', b3:'', c1:'', c2:'', c3:''},
-    $
-    winConditions = [
-    [board.a1, board.a2, board.a3],
-    [board.b1, board.b2, board.b3],
-    [board.c1, board.c2, board.c3],
-    [board.a1, board.b1, board.c1],
-    [board.a2, board.b2, board.c2],
-    [board.a3, board.b3, board.c3],
-    [board.a1, board.b2, board.c3],
-    [board.a3, board.b2, board.c1],
-    ];
+    winConditions;
+
+
 
 
 $('tbody').on('click','td', function(event){
 	var $td = $(this).closest("td");
 	$td.text(piece);
-	if(piece === 'x') {
-		piece = 'o';
+	if(piece === 'X') {
+		piece = 'O';
 	} else {
-		piece = 'x';
+		piece = 'X';
 	}
-board.a1 = $('#one').text();
-board.a2 = $('#two').text();
-board.a3 = $('#three').text();
-board.b1 = $('#four').text();
-board.b2 = $('#five').text();
-board.b3 = $('#six').text();
-board.c1 = $('#seven').text();
-board.c2 = $('#eight').text();
-board.c3 = $('#nine').text();
+
+	$('#currentMove').text('Next move: ' + piece);
+
+	board.a1 = $('#one').text();
+	board.a2 = $('#two').text();
+	board.a3 = $('#three').text();
+	board.b1 = $('#four').text();
+	board.b2 = $('#five').text();
+	board.b3 = $('#six').text();
+	board.c1 = $('#seven').text();
+	board.c2 = $('#eight').text();
+	board.c3 = $('#nine').text();
+
+  gameWinLogic(piece);
+
 });
 
 
-//check td #id for value
-//store value in board array
 
+//winning logic
+
+function gameWinLogic(piece) {
+
+	if (board.a1 === piece && board.a2 === piece && board.a3 === piece) {
+		alert(piece + ' one!');
+	} else if (board.b1 === piece && board.b2 === piece && board.b3 === piece) {
+		alert(piece + ' two!');
+	} else if (board.c1 === piece && board.c2 === piece && board.c3 === piece) {
+		alert(piece + ' three!');
+	} else if (board.a1 === piece && board.b1 === piece && board.c1 === piece) {
+		alert(piece + ' four!');
+	} else if (board.a2 === piece && board.b2 === piece && board.c2 === piece) {
+		alert(piece + ' five!');
+	} else if (board.a3 === piece && board.b3 === piece && board.c3 === piece) {
+		alert(piece + ' six!');
+	} else if (board.a1 === piece && board.b2 === piece && board.c3 === piece) {
+		alert(piece + ' seven!');
+	} else if (board.a3 === piece && board.b2 === piece && board.c1 === piece) {
+		alert(piece + ' eight!');
+	} else {
+		console.log('keep going');
+	}
+
+};
+
+winConditions = [
+  [board.a1, board.a2, board.a3],
+  [board.b1, board.b2, board.b3],
+  [board.c1, board.c2, board.c3],
+  [board.a1, board.b1, board.c1],
+  [board.a2, board.b2, board.c2],
+  [board.a3, board.b3, board.c3],
+  [board.a1, board.b2, board.c3],
+  [board.a3, board.b2, board.c1],
+];
